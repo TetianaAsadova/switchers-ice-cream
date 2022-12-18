f2cd2d31c197f29377fc6734cb64c90f778ca74d
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2022-12-18 18:00:03 +0200

    summary4

--- src/js/ingredients-modal.js
+++ src/js/ingredients-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-modal-open='111']"),
+    closeModalBtn: document.querySelector("[data-modal-close='111']"),
+    modal: document.querySelector("[data-modal='111']"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
